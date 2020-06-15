bba19b6874efc00b6d3a89b43fd73c2187404a3c
leiju <leijuxx@outlook.com>
2020-06-15 21:07:13 +0800

    修改时间计算方法

--- middleware/logToFile.go
+++ middleware/logToFile.go
@@ -13,34 +13,19 @@ import (
 func LoggerToFile() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
-		// 开始时间
 		startTime := time.Now()
-
-		// 处理请求
 		c.Next()
 
-		// 结束时间
-		endTime := time.Now()
-
-		// 执行时间
-		latencyTime := endTime.Sub(startTime)
-
-		// 请求方式
-		reqMethod := c.Request.Method
-
-		// 请求路由
-		reqURI := c.Request.RequestURI
-
-		// 状态码
-		statusCode := c.Writer.Status()
-
-		// 请求IP
-		clientIP := c.ClientIP()
+		latencyTime := time.Since(startTime).Milliseconds() // 执行时间
+		reqMethod := c.Request.Method                       // 请求方式
+		reqURI := c.Request.RequestURI                      // 请求路由
+		statusCode := c.Writer.Status()                     // 状态码
+		clientIP := c.ClientIP()                            // 请求IP
 
 		// 日志格式
 		log.Logger.WithFields(logrus.Fields{
 			"status_code":  statusCode,
-			"latency_time": latencyTime.Milliseconds(),
+			"latency_time": latencyTime,
 			"client_ip":    clientIP,
 			"req_method":   reqMethod,
 			"req_uri":      reqURI,
