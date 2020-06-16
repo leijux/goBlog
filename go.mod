393b0f91aee3e9374a1f87aace898681590641f8
leiju <leijuxx@outlook.com>
2020-06-16 18:43:08 +0800

    添加路由备注

--- go.mod
+++ go.mod
@@ -26,6 +26,6 @@ require (
 	github.com/tebeka/strftime v0.1.4 // indirect
 	github.com/valyala/fasthttp v1.14.0
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- router/v1/v1.go
+++ router/v1/v1.go
@@ -12,31 +12,32 @@ func V1(router *gin.Engine) {
 	v1 := router.Group("/v1")
 	{
 		{ //logn
-			v1.POST("/logn", apis.AddUserAPI) //注册
-			v1.GET("/emeilChack", apis.EmeilChackAPI)
+			v1.POST("/logn", apis.AddUserAPI)          //注册
+			v1.GET("/emeilChack", apis.EmeilChackAPI)  //邮箱验证api
 			v1.GET("/logn", middleware.LoginHandler()) //登入
 		}
 
-		{ //suer
+		{ //user
 			v1.DELETE("/user") //删除
 
 			v1.PUT("/user") //更新
 			v1.PATCH("/user")
 
-			v1.GET("/user", middleware.JwtMiddlewareFunc(), apis.JwtToUserAPI)
+			v1.GET("/user", middleware.JwtMiddlewareFunc(), apis.JwtToUserAPI) //根据jwt里的信息解析用户信息
 		}
-		
-		{ //验证jwt
-			v1.GET("/jwt", middleware.JwtMiddlewareFunc(), apis.JwtOkAPI) //.Use(middleware.AuthMiddleware.MiddlewareFunc())
 
+		{ //验证jwt
+			v1.GET("/jwt", middleware.JwtMiddlewareFunc(), apis.JwtOkAPI) //
 		}
 
 		{ //blog
-			v1.POST("/blog", middleware.JwtMiddlewareFunc(), apis.AddBlogAPI)
 			//添加文章
+			v1.POST("/blog", middleware.JwtMiddlewareFunc(), apis.AddBlogAPI)
+			//得到文章
 			v1.GET("/blogs", apis.GetBlogsAPI)
 			// 获取文章数量
 			v1.GET("/blogSize", apis.BlogSizeAPI)
+			//得到文章排名
 			v1.GET("/getTop", apis.GetTopAPI)
 		}
 	}
